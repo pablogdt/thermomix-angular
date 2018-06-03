c9c89d2776904a986e7868764b53fb8580f22fb4
Unknown <pablogdt@gmail.com>
2018-06-03 11:03:04 +0200

    Sumario de ingredientes

--- src/app/components/view-recipe/view-recipe.component.ts
+++ src/app/components/view-recipe/view-recipe.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ThermomixApiServiceService } from '../../services/thermomix-api-service.service';
 import { Recipe } from '../../model/recipe.model';
+import {RecipeIngredient} from '../../model/recipeIngredient.model';
 
 @Component({
   selector: 'app-view-recipe',
@@ -11,6 +12,7 @@ import { Recipe } from '../../model/recipe.model';
 export class ViewRecipeComponent implements OnInit {
   private recipeId: number;
   public recipe: Recipe;
+  public ingredientAmounts: Array<RecipeIngredient>;
 
   constructor(private route: ActivatedRoute, private router: Router, private thermomixApi: ThermomixApiServiceService) { }
 
@@ -21,10 +23,26 @@ export class ViewRecipeComponent implements OnInit {
     this.thermomixApi.getRecipe(this.recipeId).subscribe(
       (val) => {
         this.recipe = <Recipe>val;
+        this.populateTotalIngredientAmounts();
       },
       response => {
         console.log('Error getting ', response);
       });
+  }
+
+  public populateTotalIngredientAmounts() {
+    const ingredientAmounts = new Map<string, RecipeIngredient>();
+    this.recipe.steps.forEach(item => {
+      item.recipeIngredientsToAdd.forEach(recipeIngredient => {
+        const recipeIngredientSaved = ingredientAmounts.get(recipeIngredient.ingredient.name);
+        if (recipeIngredientSaved) {
+          recipeIngredientSaved.amount += recipeIngredient.amount;
+        } else {
+          ingredientAmounts.set(recipeIngredient.ingredient.name, recipeIngredient);
+        }
+      });
+    });
+    this.ingredientAmounts = Array.from(ingredientAmounts.values());
   }
 
 }
